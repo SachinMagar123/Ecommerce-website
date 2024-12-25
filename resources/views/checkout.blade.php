e6159cf9f748f0bd36125347dacf3d877e2aea04
sachin magar <gitsachin123@gmail.com>
2024-12-24 18:26:48 -0800

    extended master in checkout.blade.php

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,52 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>WATCH - Store</title>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
-        <meta content="Product Landing Page" name="keywords">
-        <meta content="Product Landing Page" name="description">
 
-        <!-- Favicon -->
-        <link href="img/favicon.ico" rel="icon">
+@extends('layouts.master')
 
-        <!-- Google Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400|Quicksand:500,600,700&display=swap" rel="stylesheet">
-
-        <!-- CSS Libraries -->
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-
-        <!-- Template Stylesheet -->
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-
-    <body>
-        <!-- Nav Start -->
-        <div id="nav">
-            <div class="container-fluid">
-                <div id="logo" class="pull-left">
-                    <a href="index.html"><img src="img/logo.png" alt="Logo" /></a>
-                </div>
-
-                <nav id="nav-menu-container">
-                    <ul class="nav-menu">
-                        <li class="menu-active"><a href="#header">Home</a></li>
-                        <li><a href="#feature">Features</a></li>
-                        <li><a href="#process">How Works</a></li>
-                        <li><a href="#products">Products</a></li>
-                        <li><a href="#testimonials">Reviews</a></li>
-                        <li><a href="#faqs">FAQs</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-        <!-- Nav End -->
-
-
-
-
+@section('content')
 
     <!-- Checkout Start -->
     <div class="container-fluid pt-5">
@@ -106,19 +61,4 @@
     </div>
     <!-- Cart End -->
 
-
-
-
-       
-        
-        <!-- JavaScript Libraries -->
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-        <script src="lib/easing/easing.min.js"></script>
-        <script src="lib/menuspy/menuspy.min.js"></script>
-        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-
-        <!-- Template Javascript -->
-        <script src="js/main.js"></script>
-    </body>
-</html>
+@endsection
